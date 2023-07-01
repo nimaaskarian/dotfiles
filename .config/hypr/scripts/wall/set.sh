fb6a4fec29c05a2421ffac2926836ab5356aeb1a
nimaaskarian <nimaas1400@gmail.com>
2023-07-01 19:14:29 +0330

    COOL VERY GUD ANIMATION I LOVE <3 <3

--- .config/hypr/scripts/wall/set.sh
+++ .config/hypr/scripts/wall/set.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-swww img -t any --transition-bezier 0.0,0.0,1.0,1.0 --transition-duration .75 --transition-step 255 --transition-fps 60 "$1"
+# swww img -t any --transition-bezier 0.0,0.0,1.0,1.0 --transition-duration .75 --transition-step 255 --transition-fps 60 "$1"
+# hyprctl hyprpaper wallpaper ",$1"
+killall swaybg 
+swaybg -m fill -i "$1" &
