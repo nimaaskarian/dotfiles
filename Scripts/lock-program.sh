a4d0ec7a968e9041c0d34260f9d196f2674bd006
nimaaskarian <nimaas1400@gmail.com>
2023-07-15 21:51:41 +0330

    lock-program, better

--- Scripts/lock-program.sh
+++ Scripts/lock-program.sh
@@ -1,38 +1,46 @@
 #!/bin/bash
-export TMPDIR=$HOME/.cache/lock-program
-ps x | pgrep -f "lock-program.sh $1" | while read -r l; do 
+DIR=$HOME/.cache/lock-program
+[ 0 -eq $# ] && {
+  echo Usage: 
+  echo "  lock-program.sh name seconds"
+  echo "  lock-program.sh 'binary name;process name' seconds"
+  exit 1
+}
+binary_name=$(printf "%s" "$1" | cut -d ';' -f 1)
+process_name=$(printf "%s" "$1" | cut -d ';' -f 2)
+
+ps x | pgrep -f "lock-program.sh $binary_name" | while read -r l; do 
   [ "$l" != $$ ] && kill "$l"
 done
-tmp=$(fd . "$TMPDIR" -e ".lock-program-$1")
 
-[ "$tmp" ] || tmp=$(date +"%Y-%m-%d.lock-program-$1")
-
-
+function mktmp() {
+  date +"$DIR/%Y-%m-%d.lock-program-$binary_name"
+}
 function reset_runtime_with_file() {
   runtime=$(cat "$tmp")
   [ "$runtime" ] || runtime=0
 }
 function write_time() {
-  echo $runtime > "$tmp"
+  echo $runtime > "$tmp" && echo "Wrote to $tmp"
   i=0
 }
+tmp=$(mktmp "$@")
 reset_runtime_with_file
 i=0
 while true; do 
   sleep 1;
-  tmp_new=$(date +"%Y-%m-%d.lock-program-$1")
-  if [ "$tmp_new" != "$tmp" ]; then
+  if [ "$(mktmp "$@")" != "$tmp" ]; then
     reset_runtime_with_file
   fi
-  if [ "$(ps -fC "$1" --no-heading)" ]; then
+  if [ "$(ps -fC "$process_name" --no-heading)" ]; then
     runtime=$((1+runtime))
     echo $runtime
     [ "$runtime" -gt "$2" ] && {
-      killall "$1" 
+      killall "$process_name" 
       write_time
     }
     i=$((i+1))
-    [ $i -gt 60 ] && {
+    [ $i -gt 30 ] && {
       write_time
     }
   fi
