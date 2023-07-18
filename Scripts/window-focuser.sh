5eac9e24d3a0ce4fd6ffd6fb2b3c60dbc2e4b338
nimaaskarian <nimaas1400@gmail.com>
2023-07-18 18:01:30 +0330

    window focus-er fix

--- Scripts/window-focuser.sh
+++ Scripts/window-focuser.sh
@@ -4,8 +4,14 @@ clients=$(hyprctl clients -j)
 classes=($(echo $clients | jq '.[] | .class'))
 addresses=($(echo $clients | jq '.[] | .address' | tr -d '"'))
 
-selected_index=$(for i in "${!classes[@]}"; do
+for i in "${!classes[@]}"; do
   striped_class=$(echo ${classes[$i]} | tr -d '"')
-[ "$striped_class" ] &&  echo "$striped_class, $(echo $clients | jq '.[] | .title' | head -n $((i+1)) | tail -n 1 | tr -d '"')";
-done | rofi -dmenu -format i -i) &&
-hyprctl dispatch focuswindow "address:${addresses[$selected_index]}"
+  [ "$striped_class" ] || {
+    unset 'classes[i]'
+    unset 'addresses[i]'
+  }
+done 
+selected_number=$(for i in "${!classes[@]}"; do echo "${classes[$i]}, $(echo $clients | jq '.[] | .title' | head -n $((i+1)) | tail -n 1 | tr -d '"')"; done| rofi -dmenu -format d -i)
+hyprctl dispatch focuswindow "address:$(for c in "${addresses[@]}"; do
+  echo $c
+done | head -n "$selected_number" | tail -n 1)"
