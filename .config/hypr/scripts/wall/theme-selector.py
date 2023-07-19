c5bda1fb287cbfca6abd07afaf626ba48b2e1514
nimaaskarian <nimaas1400@gmail.com>
2023-07-19 18:11:45 +0330

    two line theme selector

--- .config/hypr/scripts/wall/theme-selector.py
+++ .config/hypr/scripts/wall/theme-selector.py
@@ -7,7 +7,7 @@ user_themes = themes[themes.index("User Themes:")+1:themes.index("Dark Themes:")
 dark_themes = themes[themes.index("Dark Themes:")+1:themes.index("Light Themes:")]
 light_themes = themes[themes.index("Light Themes:")+1:themes.index("Extra:")]
 random_themes = ["random",  "random_dark","random_light"]
-rofi_process = subprocess.Popen(["rofi","-width","1000px","-dmenu","-format","i","-i","-markup-rows","-mesg","Type theme name:","-window-title",""],
+rofi_process = subprocess.Popen(["rofi","-sep","\x0f","-dmenu","-format","i","-i","-markup-rows","-mesg","Type theme name:","-window-title","", "-eh","2"],
                  stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 default_themes_dir=os.path.expanduser("~/.config/wal-colorschemes")
@@ -17,28 +17,30 @@ def parse_theme(theme:str,dir:str,message=""):
     colors = open(f'{dir}/{theme}.json', 'r')
     colors_data = json.load(colors)["colors"]
     color0 = colors_data[f"color{0}"]
-    output = message
+    output = ""
     for i in range(1,8):
         background_color= colors_data[f"color{i}"]
         output += f"<span background='{background_color}' foreground='{color0}'> {i} </span>"
-    return f"{output} {theme}"
+    return f"{message} {theme}\n{output}"
 
 def write_to_rofi(input:str):
-    rofi_process.stdin.write(str.encode(input+'\n'))
+    rofi_process.stdin.write(str.encode(input+'\x0f'))
 
 # rofi_list = user_themes
 for theme in random_themes:
     write_to_rofi(f"<span>{theme}</span>")
 for theme in user_themes:
-    write_to_rofi(parse_theme(theme, user_themes_dir,"User: "))
+    write_to_rofi(parse_theme(theme, user_themes_dir,"User:"))
 for theme in dark_themes:
-    write_to_rofi(parse_theme(theme, os.path.join(default_themes_dir,"dark"), "Dark: "))
+    write_to_rofi(parse_theme(theme, os.path.join(default_themes_dir,"dark"), "Dark:"))
 for (i, theme) in enumerate( light_themes ):
-    write_to_rofi(parse_theme(theme, os.path.join(default_themes_dir,"light"),"Light: "))
+    write_to_rofi(parse_theme(theme, os.path.join(default_themes_dir,"light"),"Light:"))
     light_themes[i]+=" -l"
 rofi_select_index = int(bytes.decode(rofi_process.stdout.read()))
 if rofi_select_index > 0:
     theme=(random_themes+user_themes+dark_themes+light_themes)[rofi_select_index]
     print(theme, end="")
+else:
+    print(open(os.path.expanduser("~/.cache/theme_name")).read())
 # output = rofi_process.communicate()
 
