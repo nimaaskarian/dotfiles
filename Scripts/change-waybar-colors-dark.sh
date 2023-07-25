aebf8120b9f8a1d7a622ba871ca7d26ea601475c
nimaaskarian <nimaas1400@gmail.com>
2023-07-25 21:21:33 +0330

    fixed isdark not running 2.0

--- Scripts/change-waybar-colors-dark.sh
+++ Scripts/change-waybar-colors-dark.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 nth=0
+alias isdark="~/.local/bin/isdark"
 waybar_colors_file=~/.cache/wal/colors-waybar.css
 color0=$(grep -v 'alpha' $waybar_colors_file | sed 's/.*(//;s/);//' | head -n 1 | sd ',0.75' '')
 color7=$(grep -v 'alpha' $waybar_colors_file | sed 's/.*(//;s/);//' | head -n 8 | tail -n 1)
@@ -15,7 +16,7 @@ echo light: $light_color
  grep -v 'alpha' $waybar_colors_file | sed 's/.*(//;s/);//' |
   while read -r l; do
     color=$dark_color
-    [ "$(~/.local/bin/isdark -r "$l")" -eq 1 ] && color=$light_color
+    [ "$(isdark -r "$l")" -eq 1 ] && color=$light_color
     echo "@define-color foreground$nth rgb($color);" >> $waybar_colors_file
     [ $nth -eq 3 ] && 
       echo "@define-color foreground${nth}_alpha rgba($color,0.75);" >> $waybar_colors_file
