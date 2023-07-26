c3f10172f768fbb522bf237331df97a5a1f454b5
nimaaskarian <nimaas1400@gmail.com>
2023-07-26 17:52:30 +0330

    isdark.c: added Color type, cleaner code

--- Scripts/isdark.c
+++ Scripts/isdark.c
@@ -1,26 +1,21 @@
 // vim:fileencoding=utf-8:foldmethod=marker
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <ctype.h>
 #include <math.h>
+typedef struct Color {
+  int r; 
+  int g;
+  int b;
+} Color;
 
-#define R colorInt[0]
-#define G colorInt[1]
-#define B colorInt[2]
-
-// Declarations {{{
-inline double getRgbLuma(int r,int g, int b);
-int convertDigitHexToDecimal(char hex, int nthDigit);
-int powInt(int base, int power); 
+static inline double getColorLuma(Color);
 void printUsageAndExit(char *argv[]);
-// }}}
 
 int main(int argc, char *argv[])
 {
-  char colorChars[6] = {0,0,0,0,0,0};
-  int colorInt[3] = {0,0,0};
+  Color color;
   int opt = 0;
 
   int cflag = 0, rflag = 0, lflag = 0, printIsDark = 1;
@@ -30,23 +25,7 @@ int main(int argc, char *argv[])
         if (rflag) printUsageAndExit(argv);
         cflag = 1;
 
-        int colorCount = 
-          sscanf(optarg, "#%c%c%c%c%c%c", 
-                 &colorChars[0], &colorChars[1], 
-                 &colorChars[2], &colorChars[3],
-                 &colorChars[4], &colorChars[5]);
-        if (colorCount == 3) {
-          // when we have 3 chars, we want to split them so:
-          // 0: 0,1
-          // 1: 2,3
-          // 2, 4,5
-          colorChars[4] = colorChars[2];
-          colorChars[2] = colorChars[1];
-
-          for (int i = 0; i < 6; i+=2)
-            colorChars[i+1] = colorChars[i];
-        }
-        else if(colorCount != 6)
+        if((sscanf(optarg, "#%02x%02x%02x", &color.r,&color.g,&color.b)) != 3)
           printUsageAndExit(argv);
         break;
       }
@@ -62,7 +41,7 @@ int main(int argc, char *argv[])
       case 'r': {
         if (cflag) printUsageAndExit(argv);
         rflag = 1;
-        if (sscanf(optarg, "%d,%d,%d", &R, &G, &B) != 3)
+        if (sscanf(optarg, "%d,%d,%d", &color.r, &color.g, &color.b) != 3)
           printUsageAndExit(argv);
         break;
       }
@@ -76,15 +55,7 @@ int main(int argc, char *argv[])
   if (!cflag && !rflag) {
     printUsageAndExit(argv);
   }
-  if (cflag) {
-    int j = 0;
-    for (int i = 0; i < 6; i++) {
-      colorInt[j]+=convertDigitHexToDecimal(colorChars[i],1-i%2);
-      if (i%2) 
-        j++;
-    }
-  }
-  double luma = getRgbLuma(R,G,B);
+  double luma = getColorLuma(color);
   // MIN_luma: 0
   // MAX_luma: 255
   // a color is dark when luma <= %50 * MAX_luma 
@@ -99,33 +70,14 @@ int main(int argc, char *argv[])
   return EXIT_SUCCESS;
 }
 
-inline double getRgbLuma(int r,int g, int b)
+static inline double getColorLuma(Color color)
 {
-  return sqrt(0.299*r*r+0.587*g*g+0.114*b*b);
+  return sqrt(0.299*color.r*color.r+0.587*color.g*color.g+0.114*color.b*color.b);
 }
 
 void printUsageAndExit(char *argv[])
 {
   fprintf(stderr, "Usage: %s [-c] hexcolor | [-r] r,g,b [-l]\n", argv[0]);
   exit(EXIT_FAILURE);
-}
-
-int convertDigitHexToDecimal(char hex, int nthDigit) 
-{
-  int hexInt = hex - '0';
-  if (hexInt > 9) {
-    hex = tolower(hex);
-    hexInt = hex - 'a' + 10;
-  }
-  return hexInt*powInt(16,nthDigit);
-}
-
-int powInt(int base, int power)
-{
-  int output = 1;
-  while (power--) {
-    output*=base;
-  }
-  return output;
 }
 
