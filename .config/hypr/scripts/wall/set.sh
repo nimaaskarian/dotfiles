9fa586d8fee3a9bfc49c8edb68d5678e23e6a439
nimaaskarian <nimaas1400@gmail.com>
2023-07-28 18:46:55 +0330

    why was i using swaybg?

--- .config/hypr/scripts/wall/set.sh
+++ .config/hypr/scripts/wall/set.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# swww img -t any --transition-bezier 0.65,-0.31,0.35,1.31 --transition-duration 4 --transition-step 0 --transition-fps 60 "$1"
+swww img -t any --transition-bezier 0.65,-0.31,0.35,1.31 --transition-duration 4 --transition-step 0 --transition-fps 60 "$1"
 # hyprctl hyprpaper wallpaper ",$1"
-killall swaybg 
-swaybg -m fill -i "$1" &
+# killall swaybg 
+# swaybg -m fill -i "$1" &
--- Scripts/run-to-nowhere.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#
-nohup $@ >/dev/null 2>&1 &
